c9bdaa5005eaa140a8a47c22b72ac27260f425ce
DarkRose2021 <89149647+DarkRose2021@users.noreply.github.com>
2024-02-11 09:07:37 -0700

    Working on the layout
    Added icons to the grid view, and added a promo recipe section

--- lib/main.dart
+++ lib/main.dart
@@ -18,68 +18,156 @@ class MainApp extends StatelessWidget {
       home: Scaffold(
         appBar: AppBar(
           centerTitle: true,
-          title: const Text("Recipe App"),
+          title: const Text('Recipe App'),
           actions: const [profileButton],
         ),
-        body: ListView(
+        body: Column(
           children: <Widget>[
-            GridView.count(
-              shrinkWrap: true,
-              crossAxisCount: 3,
-              physics: const ScrollPhysics(),
-              children: List.generate(
-                6,
-                (index) {
-                  String imagePath = 'assets/images/recipe.png';
-                  switch (index) {
-                    case 0:
-                      imagePath = 'assets/images/burger.png';
-                      const Text("Fast Food");
-                      break;
-                    case 1:
-                      imagePath = 'assets/images/save.png';
-                      break;
-                    case 2:
-                      imagePath = 'assets/images/heart_outline.png';
-                      break;
-                    case 3:
-                      imagePath = 'assets/images/heart_outline.png';
-                      break;
-                    case 4:
-                      imagePath = 'assets/images/heart_outline.png';
-                      break;
-                    case 5:
-                      imagePath = 'assets/images/heart_outline.png';
-                      break;
-                  }
+            const Align(
+              alignment: Alignment.centerLeft,
+              child: Padding(
+                padding: EdgeInsets.only(left: 8, top: 8),
+                child: Text(
+                  'Promo Recipe',
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 24,
+                  ),
+                ),
+              ),
+            ),
+            SingleChildScrollView(
+              scrollDirection: Axis.horizontal,
+              child: Row(
+                children: List.generate(
+                  4,
+                  (index) {
+                    return Container(
+                      margin: const EdgeInsets.all(8),
+                      padding: const EdgeInsets.all(0),
+                      height: 150,
+                      width: 300,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(6),
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.grey.withOpacity(0.5),
+                            spreadRadius: 1.5,
+                            blurRadius: 2,
+                            offset: const Offset(0, 3),
+                          ),
+                        ],
+                        image: const DecorationImage(
+                          image: AssetImage('assets/images/tempRecipeImg.jpg'),
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                      child: Stack(
+                        children: [
+                          Container(
+                            height: 500,
+                            width: 300,
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(6),
+                              color: Colors.black.withOpacity(0.3),
+                            ),
+                          ),
+                          const Align(
+                            alignment: Alignment.bottomLeft,
+                            child: Padding(
+                              padding: EdgeInsets.all(8),
+                              child: Text(
+                                'Pepperoni Grilled Cheese',
+                                style: TextStyle(
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    );
+                  },
+                ),
+              ),
+            ),
+            Expanded(
+              child: GridView.count(
+                shrinkWrap: true,
+                crossAxisCount: 3,
+                physics: const ScrollPhysics(),
+                children: List.generate(
+                  6,
+                  (index) {
+                    String imagePath = '';
+                    String label = '';
+                    switch (index) {
+                      case 0:
+                        imagePath = 'assets/images/burger.png';
+                        label = 'Fast Food';
+                        break;
+                      case 1:
+                        imagePath = 'assets/images/fruits.png';
+                        label = 'Fruits';
+                        break;
+                      case 2:
+                        imagePath = 'assets/images/mainDish.png';
+                        label = 'Main Dish';
+                        break;
+                      case 3:
+                        imagePath = 'assets/images/sweets.png';
+                        label = 'Dessert';
+                        break;
+                      case 4:
+                        imagePath = 'assets/images/vegetables.png';
+                        label = 'Vegetables';
+                        break;
+                      case 5:
+                        imagePath = 'assets/images/spaguetti.png';
+                        label = 'Pasta';
+                        break;
+                    }
 
-                  return GestureDetector(
+                    return GestureDetector(
                       onTap: () {
                         print('Box $index clicked');
                       },
                       child: Container(
-                          margin: const EdgeInsets.all(8),
-                          decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(10),
-                            color: Colors.white,
-                            boxShadow: [
-                              BoxShadow(
-                                color: Colors.grey.withOpacity(0.5),
-                                spreadRadius: 1,
-                                blurRadius: 2,
-                                offset: const Offset(0, 3),
-                              ),
-                            ],
-                          ),
-                          child: Center(
-                            child: ImageIcon(
-                              AssetImage(
-                                  imagePath),
-                              size: 70,
-                              color: Colors.black,
+                        margin: const EdgeInsets.all(8),
+                        height: 20,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(10),
+                          color: Colors.white,
+                          boxShadow: [
+                            BoxShadow(
+                              color: Colors.grey.withOpacity(0.5),
+                              spreadRadius: 1,
+                              blurRadius: 2,
+                              offset: const Offset(0, 3),
                             ),
-                          )));
-                },
+                          ],
+                        ),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Image.asset(
+                              imagePath,
+                            ),
+                            Text(
+                              label,
+                              style: const TextStyle(
+                                color: Colors.black,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    );
+                  },
+                ),
               ),
             ),
           ],
@@ -121,7 +209,7 @@ class MainApp extends StatelessWidget {
             ),
             BottomNavigationBarItem(
               icon: ImageIcon(
-                AssetImage("assets/images/recipes.png"),
+                AssetImage('assets/images/recipes.png'),
               ),
               label: 'Saved Recipes',
             ),
--- lib/theme.dart
+++ lib/theme.dart
@@ -1,21 +1,22 @@
 import 'package:flutter/material.dart';
 
 final ThemeData customTheme = ThemeData(
-    brightness: Brightness.light,
-    useMaterial3: true,
-    textTheme: const TextTheme(
-      displayLarge: TextStyle(fontSize: 32, fontWeight: FontWeight.bold),
-      bodyLarge: TextStyle(fontSize: 18, color: Colors.black87),
-    ),
-    appBarTheme: const AppBarTheme(
-      color: Colors.teal,
-      iconTheme: IconThemeData(color: Colors.white, size: 30),
-      titleTextStyle: TextStyle(fontSize: 32, color: Colors.white),
-    ),
-    bottomNavigationBarTheme: const BottomNavigationBarThemeData(
-      backgroundColor: Colors.white70,
-      selectedItemColor: Colors.teal,
-      unselectedItemColor: Colors.grey,
-    ),
-    colorScheme: ColorScheme.fromSeed(seedColor: Colors.teal),
-    iconTheme: const IconThemeData(color: Colors.white));
+  brightness: Brightness.light,
+  useMaterial3: true,
+  textTheme: const TextTheme(
+    displayLarge: TextStyle(fontSize: 32, fontWeight: FontWeight.bold),
+    bodyLarge: TextStyle(fontSize: 18, color: Colors.black87),
+  ),
+  appBarTheme: const AppBarTheme(
+    color: Colors.teal,
+    iconTheme: IconThemeData(color: Colors.white, size: 30),
+    titleTextStyle: TextStyle(fontSize: 32, color: Colors.white),
+  ),
+  bottomNavigationBarTheme: const BottomNavigationBarThemeData(
+    backgroundColor: Colors.white70,
+    selectedItemColor: Colors.teal,
+    unselectedItemColor: Colors.grey,
+  ),
+  colorScheme: ColorScheme.fromSeed(seedColor: Colors.teal),
+  iconTheme: const IconThemeData(color: Colors.white),
+);

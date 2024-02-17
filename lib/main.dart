559d7e1f870ed18b83524b1aa1a984efd5b428b1
DarkRose2021 <89149647+DarkRose2021@users.noreply.github.com>
2024-02-16 21:05:28 -0700

    I think I'm mostly happy with the homepage

--- lib/main.dart
+++ lib/main.dart
@@ -263,120 +263,114 @@ class MainApp extends StatelessWidget {
                   ),
                 ),
               ),
-              Container(
-                height: 700,
-                child: SingleChildScrollView(
-                  child: Column(
-                    children: List.generate(
-                      15,
-                      (index) {
-                        return Container(
-                          margin: const EdgeInsets.all(8),
-                          padding: const EdgeInsets.all(0),
-                          height: 150,
-                          width: 390,
-                          decoration: BoxDecoration(
-                              borderRadius: BorderRadius.circular(6),
-                              color: Colors.teal),
-                          child:  Stack(
-                            children: [
-                              Image.asset('assets/images/tempRecipeImg.jpg'),
-                              const Align(
-                                alignment: Alignment.bottomLeft,
-                                child: Padding(
-                                  padding: EdgeInsets.all(8),
-                                  child: Text(
+              SizedBox(
+                height: 500,
+                width: 390,
+                child: Column(
+                  children: List.generate(
+                    3,
+                    (index) {
+                      return Container(
+                        margin: const EdgeInsets.all(8),
+                        padding: const EdgeInsets.all(0),
+                        height: 150,
+                        width: 390,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(6),
+                          border: Border.all(color: Colors.grey.shade200),
+                          color: Colors.white,
+                          boxShadow: [
+                            BoxShadow(
+                              color: Colors.grey.withOpacity(0.5),
+                              spreadRadius: 1.5,
+                              blurRadius: 2,
+                              offset: const Offset(0, 3),
+                            ),
+                          ],
+                        ),
+                        child: Stack(
+                          children: [
+                            ClipRRect(
+                              borderRadius: const BorderRadius.only(
+                                topLeft: Radius.circular(6),
+                                topRight: Radius.circular(6),
+                              ),
+                              child: Image.asset(
+                                'assets/images/tempRecipeImg.jpg',
+                                width: 390,
+                                height: 65,
+                                fit: BoxFit.cover,
+                              ),
+                            ),
+                            const Positioned(
+                              left: 15,
+                              bottom: 35,
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Text(
                                     'Pepperoni Grilled Cheese',
                                     style: TextStyle(
-                                      color: Colors.white,
+                                      color: Colors.black,
                                       fontWeight: FontWeight.bold,
                                     ),
                                   ),
-                                ),
+                                  SizedBox(),
+                                  Row(
+                                    children: [
+                                      ImageIcon(
+                                        AssetImage(
+                                            'assets/icons/forkFilled.png'),
+                                      ),
+                                      ImageIcon(
+                                        AssetImage('assets/icons/fork.png'),
+                                      ),
+                                      ImageIcon(
+                                        AssetImage('assets/icons/fork.png'),
+                                      )
+                                    ],
+                                  )
+                                ],
                               ),
-                            ],
-                          ),
-                        );
-                      },
-                    ),
+                            ),
+                          ],
+                        ),
+                      );
+                    },
                   ),
                 ),
               ),
-            Container(
-                margin: const EdgeInsets.only(top: 10),
-                padding: const EdgeInsets.all(0),
-                height: 150,
-                width: 390,
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(6),
-                  gradient: const LinearGradient(
-                      colors: [
-                        Color(0xFF009688),
-                        Color(0xFF80CBC4),
-                      ],
-                      begin: FractionalOffset(0.0, 0.0),
-                      end: FractionalOffset(1.0, 0.0),
-                      stops: [0.0, 1.0],
-                      tileMode: TileMode.clamp),
-                  boxShadow: [
-                    BoxShadow(
-                      color: Colors.grey.withOpacity(0.5),
-                      spreadRadius: 1.5,
-                      blurRadius: 2,
-                      offset: const Offset(0, 3),
-                    ),
-                  ],
-                ),
-                child: Stack(
-                  children: [
-                    Image.asset('assets/images/foodImg.png'),
-                    const Align(
-                      alignment: Alignment.topRight,
-                      child: Padding(
-                        padding: EdgeInsets.only(top: 20, bottom: 8, right: 20),
-                        child: Text(
-                          'Add your own Meal Plan',
-                          softWrap: true,
-                          style: TextStyle(
-                            color: Colors.white,
-                            fontWeight: FontWeight.bold,
-                            fontSize: 20,
+              Align(
+                alignment: Alignment.topCenter,
+                child: Padding(
+                  padding: const EdgeInsets.only(),
+                  // ignore: avoid_print
+                  child: ButtonTheme(
+                    minWidth: 100.0,
+                    child: ElevatedButton(
+                      onPressed: () {
+                        print('Create Meal Plan Button Clicked');
+                      },
+                      style: ButtonStyle(
+                        foregroundColor:
+                            MaterialStateProperty.all<Color>(Colors.teal),
+                        backgroundColor:
+                            MaterialStateProperty.all(const Color(0xFFEEEEEE)),
+                        shape: MaterialStateProperty.all(
+                          RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(30.0),
                           ),
                         ),
-                      ),
-                    ),
-                    Align(
-                      alignment: Alignment.bottomRight,
-                      child: Padding(
-                        padding: const EdgeInsets.only(bottom: 20, right: 20),
-                        // ignore: avoid_print
-                        child: ButtonTheme(
-                          minWidth: 100.0,
-                          child: OutlinedButton(
-                            onPressed: () {
-                              print('Create Meal Plan Button Clicked');
-                            },
-                            style: ButtonStyle(
-                              foregroundColor: MaterialStateProperty.all<Color>(
-                                  Colors.white),
-                              shape: MaterialStateProperty.all(
-                                RoundedRectangleBorder(
-                                  borderRadius: BorderRadius.circular(30.0),
-                                ),
-                              ),
-                              side: MaterialStateProperty.all<BorderSide>(
-                                const BorderSide(color: Colors.white),
-                              ),
-                              minimumSize: MaterialStateProperty.all<Size>(
-                                const Size(200, 40),
-                              ),
-                            ),
-                            child: const Text('Create Meal Plan'),
-                          ),
+                        side: MaterialStateProperty.all<BorderSide>(
+                          const BorderSide(color: Colors.teal),
+                        ),
+                        minimumSize: MaterialStateProperty.all<Size>(
+                          const Size(390, 40),
                         ),
                       ),
+                      child: const Text('View all Recipes'),
                     ),
-                  ],
+                  ),
                 ),
               ),
             ],
